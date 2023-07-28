c67247882ebeda8a2dfcf21ef26c673f15ec2f40
sftqa <qa@ugurnurcan.com>
2023-07-28 19:09:14 -0400

    test fail

--- tests/mytest.js
+++ tests/mytest.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('env link', async ({ page }) => {
   const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
+  await page.goto("nonrelated");
   await page.goto(reactAppServerURL);
   await expect(page).toHaveTitle("No title");
   await expect(page).toHaveURL(/.*checkout/);
