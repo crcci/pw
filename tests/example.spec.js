b4bbefd4a1bf2a244c942c9022bed00da71bbad6
sftqa <qa@ugurnurcan.com>
2023-07-28 17:28:11 -0400

    test update

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.skip('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
 test('env link', async ({ page }) => {
   const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
   await page.goto(reactAppServerURL);
