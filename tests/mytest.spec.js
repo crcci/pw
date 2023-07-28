faa6deaa095a627ef2103cbd4ded4f4562e06e14
sftqa <qa@ugurnurcan.com>
2023-07-28 19:55:29 -0400

    test fail

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -6,3 +6,10 @@ test('test update1', async ({ page }) => {
   await expect(page).toHaveTitle("No title");
   await expect(page).toHaveURL(/.*checkout/);
 });
+
+test('test update2', async ({ page }) => {
+  const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
+  await page.goto(reactAppServerURL);
+  await expect(page).toHaveTitle("No title");
+  await expect(page).toHaveURL(/.*checkout/);
+});
