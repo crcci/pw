fe7c3bac77aa1d96376ff877c3cc7eb5ea46e96f
sftqa <qa@ugurnurcan.com>
2023-07-29 00:43:53 -0400

    test fail

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   timeout: 1 * 10 * 1000,
   testDir: './tests',
+  outputDir: 'test-results',
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
