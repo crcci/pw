fb061d3754bc3bbc8e77f84b76021cb1abf65cd0
sftqa <qa@ugurnurcan.com>
2023-07-29 00:21:04 -0400

    test fail

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -2,12 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test('test passes', async ({ page }) => {
   const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
-  await page.goto("htpp://www.apple.com");
+  await page.goto("http://www.apple.com");
 });
 
 test('test update2', async ({ page }) => {
   const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
-  await page.goto(reactAppServerURL);
+  await page.goto("htpp://react-app:3000");
   await expect(page).toHaveTitle("No title");
   await expect(page).toHaveURL(/.*checkout/);
 });
