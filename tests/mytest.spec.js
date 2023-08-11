7b210c309551bfca47c877dbfb5817408f939530
sftqa <qa@ugurnurcan.com>
2023-08-10 23:30:08 -0400

    docker file

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -3,7 +3,8 @@ const { test, expect } = require('@playwright/test');
 test('should fail now', async ({ page }) => {
   console.log("start test2");
   const reactAppServerURL = process.env.REACT_APP_SERVER_URL;
-  await page.goto("http://172.18.0.2:3000");
+  console.log(reactAppServerURL);
+  await page.goto(reactAppServerURL);
   // await expect.soft(page).toHaveTitle("No title");
   // await expect.soft(page).toHaveURL(/.*checkout/);
 });
